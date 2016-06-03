12749ee1f4f34b1c41b992520bbdde13373a73da
Coffeeteer <tgrieshiemer@gmail.com>
2016-06-03 00:47:06 -0400

    minor adjustments to the CSS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,29 @@
-var i = 120;
-function timer() {
-	var x = getElementById("timer").innerHTML = i;
-	i --;
-}
-var m = setInterval('timer()', 1000);
+$(document).ready(function() {
 
-function clear() {
-  clearInterval(m);
-}
-setTimeout('clear()', 120000);
+
+	var i = 120;
+	function timer() {
+		i --;
+		var x = document.getElementById("timer").innerHTML = i;
+		
+		if (i ===0) {
+			alert("Test is over");
+			clear();
+		}
+	}
+	var m = setInterval(timer, 1000);
+	document.getElementById("timer").innerHTML = i; 
+	console.log(i);
+	function clear() {
+	  clearInterval(m);
+
+	}
+	//setTimeout('clear()', 2000);
+
+
+});
+
+//Generate buttons dynamically using jQuery
+
+//window.reload();
+//reset();
