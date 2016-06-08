d4f4eb47659db51f7b572179757bdbe5588c1c2f
Coffeeteer <tgrieshiemer@gmail.com>
2016-06-07 20:28:27 -0400

    capturing data

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,54 @@
 $(document).ready(function() {
 
 
-// 	var i = 120;
-// 	function timer() {
-// 		i --;
-// 		var x = document.getElementById("timer").innerHTML = i;
+	var i = 120;
+	function timer() {
+		i --;
+		var x = document.getElementById("timer").innerHTML = i;
 		
-// 		if (i ===0) {
-// 			alert("Test is over");
-// 			clear();
-// 		}
-// 	}
-// 	var m = setInterval(timer, 1000);
-// 	document.getElementById("timer").innerHTML = i; 
-// 	console.log(i);
-// 	function clear() {
-// 	  clearInterval(m);
-// 	}
-// 	//setTimeout('clear()', 2000);
-// function test() {
-// 	var test = $('#tester').val();
+		if (i ===0) {
+			alert("Test is over");
+			clear();
+		}
+	}
+	var m = setInterval(timer, 1000);
+	document.getElementById("timer").innerHTML = i; 
+	console.log(i);
+	function clear() {
+	  clearInterval(m);
+	}
+	//setTimeout('clear()', 2000);
+function test() {
+	var test = $('#tester').val();
 
-// 	$('#tester').alert(test);
-// }
+	// $('#tester').alert(test);
+}
 
-
+$('#submitbutton').on('click', function() {
+	dataCapture();
+});
 
 function dataCapture(){
-	var test = [];
-	$('form input[type=radio]').each(function() {
-		if($(this).is(':checked').val()) {
-			test.push($(this).attr('value'));
-		}
-	});
+	var fname = $('#txtFirstName').val();
+	$('#lblFirstName').html(fname);
+	// var test = [];
+	// $('form input[type=radio]').each(function() {
+	// 	if($(this).is(':checked').val()) {
+	// 		test.push($(this).attr('value'));
+	// 	}
+	// });
+	var numberCorrect = $('input.cor:checked').length;
+	var numberIncorrect = 10 - numberCorrect;
+	console.log(numberCorrect);
+	console.log(numberIncorrect);
+	}
+
 	alert(test);
 	console.log(test);
 	console.log(dataCapture());
-	}
+	
 
-
-
+	
 
 
 // var correct [];
