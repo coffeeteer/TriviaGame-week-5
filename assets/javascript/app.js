e868613cd86c89f4f12e7ff2688f22a0cd91ffac
Coffeeteer <tgrieshiemer@gmail.com>
2016-12-04 22:55:51 -0500

    hit a little bit of a dead end with the CSS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 		i --;
 		var x = document.getElementById("timer").innerHTML = i;
 		
-		if (i ===0) {
+		if (i === 0) {
 			alert("Test is over");
 			clear();
 		}
@@ -17,11 +17,9 @@ $(document).ready(function() {
 	function clear() {
 	  clearInterval(m);
 	}
-	//setTimeout('clear()', 2000);
+	
 function test() {
-	var test = $('#tester').val();
-
-	// $('#tester').alert(test);
+	var test = $('#tester').val();	
 }
 
 $('#submitbutton').on('click', function() {
@@ -31,50 +29,14 @@ $('#submitbutton').on('click', function() {
 function dataCapture(){
 	var fname = $('#txtFirstName').val();
 	$('#lblFirstName').html(fname);
-	// var test = [];
-	// $('form input[type=radio]').each(function() {
-	// 	if($(this).is(':checked').val()) {
-	// 		test.push($(this).attr('value'));
-	// 	}
-	// });
+	
 	var numberCorrect = $('input.cor:checked').length;
 	var numberIncorrect = 10 - numberCorrect;
 	console.log(numberCorrect);
 	console.log(numberIncorrect);
 	}
 
-	alert(test);
 	console.log(test);
 	console.log(dataCapture());
 	
-
-	
-
-
-// var correct [];
-// var	incorrect [];
-// function store(correct, incorrect){
-// 	if(correct[] === $('.cor');) {
-// 		for(var i = ; i < correct.length; i++){
-// 			correct.push($('.cor')).alert();
-// 		}	
-// 		alert("You got " + correct);
-// 	}
-// }
-
-// function disp( divs ) {
-//   var a = [];
-//   for ( var i = 0; i < divs.length; i++ ) {
-//     a.push( divs[ i ].innerHTML );
-//   }
-//   $( "span" ).text( a.join( " " ) );
-// }
- 
-// disp( $( "div" ).toArray().reverse() );
-
 });
-
-//Generate buttons dynamically using jQuery
-
-//window.reload();
-//reset();
